df621c1234bc1dad1429c0b81cd605654b2f22dd
Bishwas Gautam <bgautam@bloominsurance.com>
2022-06-07 19:51:24 -0400

    Initial commit

---
+++ src/combinedLists.js
@@ -0,0 +1,9 @@
+function combineMonthlyRainfallTotals(firstMonthTotals, secondMonthTotals) {
+  // Write your solution to combine the Lists of Monthly Rainfall Totals here.
+  // The correct result will not have duplicates and will appropriately add rainfall amounts from cities that appear in both lists.
+}
+
+
+
+
+module.exports = combineMonthlyRainfallTotals;
